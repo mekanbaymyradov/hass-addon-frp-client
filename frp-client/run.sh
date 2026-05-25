a7dcd7874f3ddb11848a6a02840d7be99d2e209d
mekanbaymyradov <baymyradov.mekanx@gmail.com>
2026-05-25 13:38:46 +0500

    Add healthceks and simplify the scripts

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bashio
-WAIT_PIDS=()
-CONFIG_PATH='/share/frpc.toml'
-DEFAULT_CONFIG_PATH='/frpc.toml'
-
-function stop_frpc() {
-    bashio::log.info "Shutdown frpc client"
-    kill -15 "${WAIT_PIDS[@]}"
-}
+CONFIG_PATH='/frpc.toml'
 
 bashio::log.info "Reading add-on configuration..."
 
@@ -18,19 +11,9 @@ export FRP_PROXY_NAME="$(bashio::config 'proxyName')"
 export LOCAL_PORT="$(bashio::config 'localPort')"
 export REMOTE_PORT="$(bashio::config 'remotePort')"
 
-# Copy the template over only if it doesn't already exist in /share
-if [ ! -f "$CONFIG_PATH" ]; then
-    bashio::log.info "Creating initial frpc.toml from template."
-    cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
-else
-    bashio::log.info "Using existing configuration at $CONFIG_PATH"
-fi
 
 bashio::log.info "Starting frp client"
 
 cd /usr/src
-# Run FRP; it will automatically evaluate the template tags in frpc.toml
-./frpc -c $CONFIG_PATH & WAIT_PIDS+=($!)
 
-trap "stop_frpc" SIGTERM SIGHUP
-wait "${WAIT_PIDS[@]}"
+exec ./frpc -c $CONFIG_PATH
